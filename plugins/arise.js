1a239399089554d5991ca6ac0517d84c089b8d53
ABRAHAM <120536940+abrahamdw882@users.noreply.github.com>
2026-01-18 21:49:45 +0000

    Update bot details and image URL in arise.js

--- plugins/arise.js
+++ plugins/arise.js
@@ -15,10 +15,10 @@ module.exports = {
 
         if (isTriggered) {
             const info = '*BOT ACTIVE AND RUNNING...*';
-            const imgUrl = 'https://i.ibb.co/65fwTVG/carbon-3.png';
-            const author = 'ABZTech';
-            const botname = 'ABZTech ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ';
-            const sourceUrl = 'https://abztech.xyz/';
+            const imgUrl = 'https://files.catbox.moe/uz899q.jpg';
+            const author = 'XLIOCN V2';
+            const botname = 'XLIOCN V2 ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ';
+            const sourceUrl = 'https://abztech.my.id/';
 
             try {
                 const thumbnailBuffer = (await axios.get(imgUrl, { responseType: 'arraybuffer' })).data;
