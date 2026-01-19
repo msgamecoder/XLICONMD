5952a774f646e214e7b95df1a848e3271c9595d1
EliteProTech <proelite455@gmail.com>
2026-01-19 06:12:05 +0100

    Rename global.prefix to global.BOT_PREFIX

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 global.sessionid = process.env.SESSION_ID || '';
-global.prefix = '.';
+global.BOT_PREFIX = '.';
 global.owners = [
     '25770239992037@lid',
     '233533763772@s.whatsapp.net'
